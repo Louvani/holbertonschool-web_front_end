a4d11710defdc0e398cdf34868d1b2c3de11e7f8
Louvani <paula.louvani@gmail.com>
2022-01-26 04:52:49 -0500

    tasks 9-14

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let prime = 2; prime <= 100; prime++) {
+        let isPrime = true;
+        for (let divisor = 2; divisor < prime; divisor++) {
+            if (prime % divisor === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const startTime = performance.now();
+
+for (let i = 0; i < 100; i++) {
+    setTimeout(function() {
+        countPrimeNumbers();
+      }, 0);
+}
+
+const endTime = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was  ${endTime - startTime} milliseconds.`);
